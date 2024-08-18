b6e5317cc371797832bc24c43e6a75a0a22d6aa7
Theo Macbook <theo@terrasunny.com>
2024-08-18 14:34:59 +0100

    first update

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="contact-cards" class="col-md-3 mt-4 stretch-card transparent">
+        <div class="card card-tale">
+            <div class="card-body">
+                <img src="{{ asset('images/faces/face28.jpg') }}" alt="profile" />
+                <p class="mt-4">Theo Georgewill</p>
+                <p class="">Phone</p>
+                <p>Email</p>
+            </div>
+        </div>
+    </div>
+@endsection       
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/customers', function () {
+    return view('clients.index');
+});
+
 Route::get('/services', function () {
     return view('services.index');
 });
