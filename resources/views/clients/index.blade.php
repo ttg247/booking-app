d8799ce8478acca9ba4a1decfd32235874f3e085
Theo Macbook <theo@terrasunny.com>
2024-08-18 14:58:21 +0100

    finished contact card mvp

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="contact-cards" class="col-md-3 mt-4 stretch-card transparent">
-        <div class="card card-tale">
-            <div class="card-body">
-                <img src="{{ asset('images/faces/face28.jpg') }}" alt="profile" />
-                <p class="mt-4">Theo Georgewill</p>
-                <p class="">Phone</p>
-                <p>Email</p>
+    <div id="contact-cards" class="row">
+        <div  class="col-md-3 mt-4 mb-4 stretch-card transparent">
+            <div class="card card-tale">
+                <div class="card-body">
+                    <img class="contact-profile" src="{{ asset('images/faces/face28.jpg') }}" alt="profile" />
+                    <p class="mt-2">Theo Georgewill</p>
+                    <p class="">Phone</p>
+                    <p>Email</p>
+                </div>
             </div>
         </div>
     </div>
